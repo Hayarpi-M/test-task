395109f64628e2ca70e83abba51a4240c0bc483f
Hayarpi-M <70703912+Hayarpi-M@users.noreply.github.com>
2025-02-28 22:34:42 +0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,42 +8,26 @@ window.addEventListener("scroll", function() {
   }
 });
 
-(function($) {
-    /* fixed menu */
-    $(document).scroll(function() {
-      if ( $(document).scrollTop() >= 30 ) {
-          $('.header').addClass('scrolled');
-      } else if ( $(document).scrollTop() >= -30 ) {
-          $('.header').removeClass('scrolled');
-      }
-    });
 
-    /* Form validation */
-    
-    /*Form validation end*/
+/* Menu burger */
+document.addEventListener("DOMContentLoaded", function () {
+    let menuBurger = document.querySelector(".menu-burger");
+    let menuClose = document.querySelector(".menu-close");
+    let headerMenu = document.querySelector(".header-menu");
+    let body = document.body;
 
-    /* Menu burger */
-    document.addEventListener("DOMContentLoaded", function () {
-      let menuBurger = document.querySelector(".menu-burger");
-      let menuClose = document.querySelector(".menu-close");
-      let headerMenu = document.querySelector(".header-menu");
-      let body = document.body;
-  
-      if (menuBurger) {
-          menuBurger.addEventListener("click", function () {
-              console.log("hii");
-              headerMenu.classList.add("active");
-              body.classList.add("body-fixed");
-          });
-      }
-  
-      if (menuClose) {
-          menuClose.addEventListener("click", function () {
-              headerMenu.classList.remove("active");
-              body.classList.remove("body-fixed");
-          });
-      }
-  });
+    if (menuBurger) {
+        menuBurger.addEventListener("click", function () {
+            console.log("hii");
+            headerMenu.classList.add("active");
+            body.classList.add("body-fixed");
+        });
+    }
 
-    
-})(jQuery);
+    if (menuClose) {
+        menuClose.addEventListener("click", function () {
+            headerMenu.classList.remove("active");
+            body.classList.remove("body-fixed");
+        });
+    }
+});
